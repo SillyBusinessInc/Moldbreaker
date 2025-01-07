699f92949c4413b09ef771c8ea476b178272a4d0
Pim van Milt <pim.milt@gmail.com>
2025-01-07 12:10:33 +0100

    Deathzone script and moving Tessa's spawn point code to respawn code.
    Moved to Tessa's respawn code to SpawnPoint.cs (from PlayerSpawnPoint.cs) and made PlayerSpawnPoint inherit from SpawnPoint.
    This is done so that i could reuse the code, but leave out the start of a level specific code that wasnt needed for the spawnpoints i needed.

--- Assets/Global/Scripts/Deathzone.cs
+++ Assets/Global/Scripts/Deathzone.cs
@@ -8,39 +8,55 @@ public class Deathzone : MonoBehaviour
     [Tooltip("The amount of damage , as % of maxhealth, to deal to the player when hitting this Deathzone")]
     private float damageAmount = 0.25f;
     [SerializeField]
-    private PlayerSpawnPoint spawnPoint;
+    private SpawnPoint spawnPoint;
+    private CrossfadeController crossfadeController;
+    private PlayerReference playerReference;
+
+    void Start()
+    {
+        crossfadeController = GlobalReference.GetReference<CrossfadeController>();
+        playerReference = GlobalReference.GetReference<PlayerReference>();
+    }
     void OnTriggerEnter(Collider other)
     {
         //check for player
         other.TryGetComponent<PlayerObject>(out PlayerObject player);
-
         if (player)
         {
-            Debug.Log("PLAYER COLLISION DZ");
-            Player playerRef = GlobalReference.GetReference<PlayerReference>().Player;
-            Debug.Log($"Do {damageAmount * 100}% damage");
-            playerRef.OnHit(playerRef.playerStatistic.MaxHealth.GetValue() * damageAmount, new Vector3(0, 0, 0));
-            StartCoroutine(DelayRespawn());
+            StartCoroutine(PlayerRepsawnRoutine());
             return;
         }
-
         //check for enemy
         other.TryGetComponent<EnemiesNS.EnemyBase>(out EnemiesNS.EnemyBase enemy);
 
         if (enemy)
         {
-            // can enemies even fall? they dont have a RigidBody, due to navmeshagent.
-            Debug.Log("ENEMY COLLISION DZ");
             // deal damage to equal the amount of MAX health times 2 just to be sure.
             enemy.OnHit(enemy.maxHealth * 2);
             return;
         }
     }
 
+    private IEnumerator PlayerRepsawnRoutine()
+    {
+        // get the actual player object to damage.
+        Player playerRef = playerReference.Player;
+        playerRef.OnHit(playerRef.playerStatistic.MaxHealth.GetValue() * damageAmount, new Vector3(0, 0, 0));
+
+        // start the crossfade
+        if (crossfadeController) yield return StartCoroutine(crossfadeController.Crossfade_Start());
+
+        // cancel out any movementinput and respawn the player on predefined spot
+        playerRef.movementInput = new Vector2(0, 0);
+        yield return StartCoroutine(DelayRespawn());
+
+        // clear the crossfade
+        if (crossfadeController) yield return StartCoroutine(crossfadeController.Crossfade_End());
+    }
+
     private IEnumerator DelayRespawn()
     {
         yield return new WaitForSecondsRealtime(0.5f);
-        Debug.Log($"Respawn in predefined spot: {spawnPoint.transform.position}");
-        spawnPoint.SpawnPoint();
+        spawnPoint.Spawn();
     }
 }
