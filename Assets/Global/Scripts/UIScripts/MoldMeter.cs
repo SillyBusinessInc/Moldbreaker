f88f28df8efee215fc69016fd20e1fcff362dcb6
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-01-18 20:47:31 +0100

    it works, without testing

--- Assets/Global/Scripts/UIScripts/MoldMeter.cs
+++ Assets/Global/Scripts/UIScripts/MoldMeter.cs
@@ -2,47 +2,49 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 
-public class HealthMeter : MonoBehaviour
+public class MoldMeter : MonoBehaviour
 {
-    private Player player;
     [SerializeField] private TMP_Text HealthPercentageText;
     [SerializeField] private float healthPercentage;
     [SerializeField] private RectTransform HealthMeterImage;
-    private Coroutine moveCoroutine;
     [SerializeField] private float animationDuration = 0.5f; // Duration for smooth movement
     [SerializeField] private GameObject mold;
-    private float savedHealthPercentage = -1;
-    private float PlayerMaxHealth = -1;
     
-
+    private Coroutine moveCoroutine;
+    private float savedHealthPercentage = -1;
+    private float playerMaxHealth = -1;
+    private Player player;
+    
     void Awake()
     {
-        GlobalReference.SubscribeTo(Events.MOLDMETER_CHANGED, UpdateHealthMeter);
+        GlobalReference.SubscribeTo(Events.MOLDMETER_CHANGED, this.UpdateMeter);
         mold.SetActive(false);
     }
 
     void Start() {
         player = GlobalReference.GetReference<PlayerReference>().Player;
-        Vector3 currentScale = HealthMeterImage.localScale;
-        HealthMeterImage.localScale = new Vector3(-currentScale.x, currentScale.y, currentScale.z);
+        var currentScale = HealthMeterImage.localScale;
+        HealthMeterImage.localScale = new(-currentScale.x, currentScale.y, currentScale.z);
     }
 
-    void Update() => UpdateHealthMeter();
+    void Update() => this.UpdateMeter();
 
-    public void UpdateHealthMeter()
+    public void UpdateMeter()
     {
-        if (PlayerMaxHealth == -1) PlayerMaxHealth = player.playerStatistic.Health;
-        healthPercentage = player.playerStatistic.Health / PlayerMaxHealth * 100;
+        if (this.playerMaxHealth == -1) this.playerMaxHealth = player.playerStatistic.Health;
+        healthPercentage = player.playerStatistic.Health / this.playerMaxHealth * 100;
         if (savedHealthPercentage == healthPercentage) return;
         savedHealthPercentage = healthPercentage;
-        
-        if (healthPercentage != 100 ) mold.SetActive(true);
-        else  mold.SetActive(false);
-        HealthPercentageText.text = healthPercentage.ToString() + '%';
 
-        float barWidth = GetComponent<RectTransform>().rect.width;
-        float targetPosX = healthPercentage / 100 * barWidth;
-        Vector2 targetPosition = new(targetPosX, HealthMeterImage.anchoredPosition.y);
+        // If the percentage is 100% (or more for that matter),
+        // it still show a little artifact of the mold which we dont want. and so that's why we turn it off
+        this.mold.SetActive(this.healthPercentage < 100);
+        
+        HealthPercentageText.text =  $"{healthPercentage}%";
+
+        var barWidth = GetComponent<RectTransform>().rect.width;
+        var targetPosX = healthPercentage / 100 * barWidth;
+        var targetPosition = new Vector2(targetPosX, HealthMeterImage.anchoredPosition.y);
 
         // start smooth movement
         if (moveCoroutine != null)
@@ -54,13 +56,13 @@ public class HealthMeter : MonoBehaviour
 
     private IEnumerator SmoothMove(RectTransform rect, Vector2 target, float duration)
     {
-        Vector2 startPosition = rect.anchoredPosition;
-        float elapsedTime = 0f;
+        var startPosition = rect.anchoredPosition;
+        var elapsedTime = 0f;
 
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / duration);
+            var t = Mathf.Clamp01(elapsedTime / duration);
             rect.anchoredPosition = Vector2.Lerp(startPosition, target, t);
             yield return null;
         }
