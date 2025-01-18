a16fdbe9c9724a1f87867cfb8d9caa09695a6b1c
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-01-18 19:45:18 +0100

    spawner completely removed

--- Assets/Global/Scripts/DELETE THESE SCRIPTS/EnemySpawner/WavePart.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-[System.Serializable]
-public class WavePart
-{
-    public List<EnemyPrefabCount> enemyPrefabs; // List to replace dictionary
-    public int spawnArea; // Possible spawn areas for this wave
-    public bool center = false; // Center the spawn area
-}
