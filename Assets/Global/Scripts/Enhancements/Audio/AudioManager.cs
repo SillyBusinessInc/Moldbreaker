5d4036eef84e88c2a373567532f15bfe49c1a3bb
fabio <95278302+fabiothomas@users.noreply.github.com>
2025-01-21 11:24:50 +0100

    Fixed settings and audio manager

--- Assets/Global/Scripts/Enhancements/Audio/AudioManager.cs
+++ Assets/Global/Scripts/Enhancements/Audio/AudioManager.cs
@@ -5,11 +5,10 @@ public class AudioManager : MonoBehaviour
 {
     [Header("Audio Sounds")]
     public Sound[] musicSounds, sfxSounds;
-
-    [HideInInspector]
-    public static AudioManager Instance;
-
     public AudioMixer audioMixer;
+
+    [HideInInspector] public static AudioManager Instance;
+
     void Awake()
     {
         if (Instance != null) Destroy(gameObject);
@@ -19,9 +18,8 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        LoadAllSettings();
+        LoadFromLocal();
     }
-
 
     public void UpdateMusicVolume(float volume) => UpdateAudio("Music", volume);
     public void UpdateSFXVolume(float volume) => UpdateAudio("SFX", volume);
@@ -33,7 +31,6 @@ public class AudioManager : MonoBehaviour
     public void PlayMusicOnRepeat(string name, Vector3? location = null) => PlaySound(name, true, true, location);
     public void PlaySFX(string name, Vector3? location = null) => PlaySound(name, false, false, location);
     public void PlaySFXOnRepeat(string name, Vector3? location = null) => PlaySound(name, true, false, location);
-
 
     public void StopMusicSound(string name)
     {
@@ -49,24 +46,20 @@ public class AudioManager : MonoBehaviour
         s.audioSource.Stop();
     }
 
-
     private void UpdateAudio(string mixerString, float volume)
     {
         float normalizedVolume = Mathf.Clamp01(volume / 8.0f);
         float dB = normalizedVolume > 0 ? Mathf.Lerp(-80, 0, Mathf.Log10(1 + 9 * normalizedVolume) / Mathf.Log10(10)) : -80;
         audioMixer.SetFloat(mixerString, dB);
         AudioSettingSave audioSettingSave = GlobalReference.AudioSettingSave;
-        audioSettingSave.LoadAll();
 
         audioSettingSave.Set(mixerString, volume);
-        audioSettingSave.SaveAll();
     }
 
 
     private float GetVolume(string param)
     {
         AudioSettingSave audioSettingSave = GlobalReference.AudioSettingSave;
-        audioSettingSave.LoadAll();
         return audioSettingSave.Get<float>(param);
     }
 
@@ -88,16 +81,13 @@ public class AudioManager : MonoBehaviour
         s.audioSource.clip = s.clip;
         s.audioSource.loop = repeat;
         s.audioSource.Play();
-
     }
 
-    private void LoadAllSettings()
+    public void LoadFromLocal()
     {
         AudioSettingSave audioSettingSave = GlobalReference.AudioSettingSave;
-        audioSettingSave.LoadAll();
         UpdateMusicVolume(audioSettingSave.Get<float>("Music"));
         UpdateSFXVolume(audioSettingSave.Get<float>("SFX"));
         UpdateMasterVolume(audioSettingSave.Get<float>("Master"));
     }
-
 }
