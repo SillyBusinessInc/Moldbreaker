4c6db9085c0965ea58c13aaaa07a86303ecee635
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-01-21 15:36:13 +0100

    transition can happen

--- Assets/Global/Scripts/RoomLogic/CrossfadeController.cs
+++ Assets/Global/Scripts/RoomLogic/CrossfadeController.cs
@@ -5,13 +5,19 @@ public class CrossfadeController : Reference
 {
     public Animator animator;
     private float transitionTime = 1f;
+    private string lastState = "end";
 
-    public IEnumerator Crossfade_Start() {
-        animator.SetTrigger("start");
+    public IEnumerator Crossfade_Start()
+    {
+        var newState = "start";
+        if (lastState == newState) yield break;
+        lastState = newState;
+        animator.SetTrigger(newState);
         yield return new WaitForSeconds(transitionTime);
     }
 
     public IEnumerator Crossfade_End() {
+        lastState = "end";
         animator.SetTrigger("end");
         yield return new WaitForSeconds(transitionTime);
     }
