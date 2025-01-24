c30fe9e6b870b61f4d494a458bd257c28f08eb10
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-01-24 10:46:55 +0100

    add resolution

--- Assets/Global/Scripts/Singletons/Settings.cs
+++ Assets/Global/Scripts/Singletons/Settings.cs
@@ -4,6 +4,7 @@ public class Settings : SaveSystem
 
     public override void Init() {
         Add("screen_mode", 0);
+        Add("resolution", 0);
         
         Add("master_volume", 0.5f);
         Add("effects_volume", 0.5f);
